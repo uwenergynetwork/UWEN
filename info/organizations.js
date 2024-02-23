d45b598d30e5ccbff786399f981411434f353aaa
owen-sellner <owen.sellner@gmail.com>
2024-02-23 10:06:26 -0500

    Add organization info

--- info/organizations.js
+++ info/organizations.js
@@ -1,12 +1,37 @@
 export const organizations = [
     {
-        "title": "Ontario Power Generation (OPG)",
-        "description": "Ontario Power Generation (OPG) is Ontario's largest clean electricity generator, OPG produces about half of the province's power using a diverse portfolio of publically-owned generating assets. OPG regularly posts jobs on WaterlooWorks for 4-month and 8-month co-ops so be sure to check them out!",
-        "imageFile": "ontario-power-generation.webp",
+        "title": "Affordable Energy for Humanity (AE4H)",
+        "description": "AE4H is a program organized by the Waterloo Institute for Sustainable Energy (WISE)  which provides international internship opportunities with clean energy organizations across the world. More information can be found at this link or by emailing Ambika Opal at WISE, who is in charge of the AE4H initiative.",
+        "imageFile": "ae4h.webp",
+    },
+    {
+        "title": "Office of Energy Research & Development",
+        "description": "The Government of Canada regularly posts jobs for students and recent graduates in the Energy sector! During one of our EnerChats, Dr.Bronwyn Lazowski, a senior policy analyst at the Office of Energy Research & Development mentioned many resources which students should access if interested in working in the field. If you have any additional questions regarding this work, please reach out to us and we can put you in contact with Dr.Bronwyn Lazowski.",
+        "imageFile": "office-of-energy-research.webp",
     },
     {
         "title": "Ontario Power Generation (OPG)",
         "description": "Ontario Power Generation (OPG) is Ontario's largest clean electricity generator, OPG produces about half of the province's power using a diverse portfolio of publically-owned generating assets. OPG regularly posts jobs on WaterlooWorks for 4-month and 8-month co-ops so be sure to check them out!",
         "imageFile": "ontario-power-generation.webp",
+    },
+    {
+        "title": "Liberty Utilities",
+        "description": "Liberty Utilities is a regulated water, natural gas and electric transmission and distribution utility, delivering responsive and reliable essential services to nearly 800,000 customers in 13 states across the United States and New Brunswick in Canada. Liberty Utilities hires students for various roles such as accounting, finance, engineering and more! They post co-op opportunities on WaterlooWorks and go into detail on their website, check them out!",
+        "imageFile": "liberty-utilities.webp",
+    },
+    {
+        "title": "Independent Electricity System Operator",
+        "description": "The Independent Electricity System Operator (IESO) works with Ontario's power system. The IESO delivers key services across the electricity sector including: managing the power system in real-time, planning for the province's future energy needs, enabling conservation and designing a more efficient electricity marketplace to support sector evolution. The IESO posts co-op opportunities on WaterlooWorks and often rehires their students as full-time employees! Check them out!",
+        "imageFile": "ieso.webp",
+    },
+    {
+        "title": "Kinectrics",
+        "description": "Kinectrics is an Electricity Lifecycle Solutions Company which develops solutions for global energy and medical industries. In Canada, they offer 4, 8, and 12-month co-op work terms all year-round, as well as 16-month Professional Experience Year (PEY) placements starting in May in select areas. They post their co-op opportunities on WaterlooWorks but check out them out for more information!",
+        "imageFile": "kinectrics.webp",
+    },
+    {
+        "title": "Guidehouse",
+        "description": "Guidehouse is a management consulting firm which focuses on multiple sectors ranging from health care, state and local government, energy and more. The Toronto office is centered around Energy Providers services and works with clients across North America. Every summer Guidehouse hires graduating students to participate in their interactive internship. Summer internship jobs are posted as early as October, so be sure to check early!",
+        "imageFile": "guidehouse.webp",
     },
 ];
