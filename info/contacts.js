5c28e80054c19e7f7e82ac5d67116bf51a5bab2b
owen-sellner <owen.sellner@gmail.com>
2024-02-23 09:51:00 -0500

    Add contact info

--- info/contacts.js
+++ info/contacts.js
@@ -3,12 +3,24 @@ export const contacts = [
         "title": "Zeynep Celik",
         "description": "Zeynep Celik is currently a Technical Specialist at Solargis and is concurrently pursuing a Master's degree in Energy Conversion Systems & Technologies at the University of Rovira i Virgili (URV). With a background in Physics & Astronomy, Zeynep assists customers in effectively utilizing satellite-based solar data and ground measurements.",
         "imageFile": "zeynep-celik.webp",
-        "link": ""
+        "link": "https://www.linkedin.com/in/szeynepcelik/"
     },
     {
-        "title": "Ontario Power Generation (OPG)",
-        "description": "Ontario Power Generation (OPG) is Ontario's largest clean electricity generator, OPG produces about half of the province's power using a diverse portfolio of publically-owned generating assets. OPG regularly posts jobs on WaterlooWorks for 4-month and 8-month co-ops so be sure to check them out!",
-        "imageFile": "zeynep-celik.webp",
-        "link": "https://www.linkedin.com/in/szeynepcelik/"
+        "title": "Haida Liu",
+        "description": "Haida Liu, P.Eng., PMP, is a Program Management Consultant at Toronto Hydro with a background in mechanical and industrial engineering. In this role, she specializes in managing programs in the energy sector, focusing on mechanical testing and condition assessment of overhead line components. Haida actively contributes to promoting the role of women in the renewable energy field as an Advisory Committee Member at Women in Renewable Energy (WiRE), leading the Academic Committee and collaborating on events and conferences that support women in the industry.",
+        "imageFile": "haida-liu.webp",
+        "link": "https://www.linkedin.com/in/haidaliu/"
+    },
+    {
+        "title": "Kevin Campbell",
+        "description": "Kevin Campbell joined EDF Renewables in 2010 and currently holds the title of Director - Development.  In this role, Kevin is the project lead during the development & permitting phase of various utility scale renewable energy projects across New York State. Kevin has 13 years of solar and wind development experience in Canada and the United States and currently oversees a development portfolio of 1,000MW of solar, energy storage and wind projects in New York State. Kevin sits on the NYSERDA Agricultural Technical Working Group and is on the Advisory Board of the American Solar Grazing Association.",
+        "imageFile": "kevin-campbell.webp",
+        "link": "https://www.linkedin.com/in/krcampbell/"
+    },
+    {
+        "title": "Alex Ripley",
+        "description": "Alex Ripley currently serves as a Commercial Energy Advisor at the Ministry of Foreign Affairs of Denmark, based in Toronto, Canada. In this role, he works within the Trade Council North America's Energy & Environment group, offering in-market export development services to Danish companies operating in the United States and Canada, with a focus on waste and biogas solutions.",
+        "imageFile": "alex-ripley.webp",
+        "link": "https://www.linkedin.com/in/alexripley/"
     },
 ];
