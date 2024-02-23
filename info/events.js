ac2e54e0b2179e0527c82d407788fa1533a8e5ae
owen-sellner <owen.sellner@gmail.com>
2024-02-23 10:45:40 -0500

    Update banner photo

---
+++ info/events.js
@@ -0,0 +1,8 @@
+export const organizations = [
+    {
+        "date": "2024-",
+        "title": "Affordable Energy for Humanity (AE4H)",
+        "description": "AE4H is a program organized by the Waterloo Institute for Sustainable Energy (WISE)  which provides international internship opportunities with clean energy organizations across the world. More information can be found at this link or by emailing Ambika Opal at WISE, who is in charge of the AE4H initiative.",
+        "imageFile": "ae4h.webp",
+    },
+];
